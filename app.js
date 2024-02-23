9af968748c584de2a73b2257773bb6e7ad8a9221
Carlos Henrique Sousa de Macedo <carlossousa@sistemafieto.rede>
2024-02-22 21:47:00 -0300

    fiz a primeira rota de usuário

--- app.js
+++ app.js
@@ -1,50 +1,12 @@
 const express = require('express')
 const app = express();
-const usuario = [
-    {
-    id:1,
-    nome: "Bleno",
-    email:"bleno@gmail.com",
-    senha:"123",
-    },
-    {
-        id:2,
-        nome: "Felipe",
-        email:"felipe@gmail.com",
-        senha:"123",       
-    },
-    {
-        id:3,
-        nome: "Nero",
-        email:"nero@gmail.com",
-        senha:"123",   
-    },
-    {
-        id:4,
-        nome: "carlos",
-        email:"carlos@gmail.com",
-        senha:"123",   
-    }
-]
-app.get("/",(req,res,next)=>{
-    res.json(usuario)
-})
-app.get("/usuario",(req,res,next)=>{
-    let nomes=[];
-    usuario.map((linha)=>{
-        nomes.push({
-            nome:linha.nome,
-            email:linha.email
-        })
-    })
+app.use(express.json());
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
-    res.json(nomes)
-})
-app.post("/usuario",(req,res,next)=>{
-    const {id} = req.body;
-    console.log(id)
+const rotaUsuarios = require("./routes/rotaUsuario");
 
-});
 
+app.use("/usuario",rotaUsuarios);
 
 module.exports = app
